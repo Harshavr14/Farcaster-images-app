6ae9a91fd44b29d6368264d7aea12d55d57f6f22
Harsha <kharshavardhan2k3@gmail.com>
2024-03-20 18:45:46 +0530

    frames-images

---
+++ README.md
@@ -0,0 +1,2 @@
+# Farcaster Frames - Demo pictures with advanced where need to give email for viewing images, other is simple can view images with basic images rendering
+
---
+++ src/app/advanced/page.tsx
@@ -0,0 +1,39 @@
+import { getFrameMetadata } from '@coinbase/onchainkit/frame'
+import type { Metadata } from 'next'
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Sign Up!',
+    },
+  ],
+  image: {
+    src: `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
+  },
+  input: {
+    text: 'Your Email Please',
+  },
+  postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/advanced`,
+})
+
+export const metadata: Metadata = {
+  title: 'Advanced Frame',
+  description: 'Another, more advanced frame example',
+  openGraph: {
+    title: 'Advanced Frame',
+    description: 'Another, more advanced frame example',
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`],
+  },
+  other: {
+    ...frameMetadata,
+    'of:accepts:xmtp': '2024-02-01',
+  },
+}
+
+export default function Page() {
+  return (
+    <>
+      <h1>Advanced Frame</h1>
+    </>
+  )
+}
